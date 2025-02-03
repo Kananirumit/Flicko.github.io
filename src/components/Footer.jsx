3c950d92e749c30ae5df8a8a08d0b3e556c65fcc
Rumit kanani <144870993+Kananirumit@users.noreply.github.com>
2025-02-03 18:23:49 +0530

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,66 +1,61 @@
 import React from 'react';
+import { Container, Row, Col, Image } from 'react-bootstrap';
 
 export default function Footer() {
-  return (
-    <div>
-      <footer className="footer">
-        <div className="container">
-          <div className="row">
-            <div className="col-12 col-md-8 order-md-1 col-lg-12 col-xl-4">
-              <div className="footer__logo">
-                <img src="img/logo.svg" alt="Logo" />
-              </div>
-              <p className="footer__tagline">
-                Movies & TV Shows, Online cinema, <br /> Movie database HTML Template. <br /> The perfect choice for your project.
-              </p>
-              <ul className="footer__social">
-                <li><a href="#"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M15.12,5.32H17V2.14A26.11,26.11,0,0,0,14.26,2C11.54,2,9.68,3.66,9.68,6.7V9.32H6.61v3.56H9.68V22h3.68V12.88h3.06l.46-3.56H13.36V7.05C13.36,6,13.64,5.32,15.12,5.32Z"></path></svg></a></li>
-                <li><a href="#"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M17.34,5.46h0a1.2,1.2,0,1,0,1.2,1.2A1.2,1.2,0,0,0,17.34,5.46Zm4.6,2.42a7.59,7.59,0,0,0-.46-2.43,4.94,4.94,0,0,0-1.16-1.77,4.7,4.7,0,0,0-1.77-1.15,7.3,7.3,0,0,0-2.43-.47C15.06,2,14.72,2,12,2s-3.06,0-4.12.06a7.3,7.3,0,0,0-2.43.47A4.78,4.78,0,0,0,3.68,3.68,4.7,4.7,0,0,0,2.53,5.45a7.3,7.3,0,0,0-.47,2.43C2,8.94,2,9.28,2,12s0,3.06.06,4.12a7.3,7.3,0,0,0,.47,2.43,4.7,4.7,0,0,0,1.15,1.77,4.78,4.78,0,0,0,1.77,1.15,7.3,7.3,0,0,0,2.43.47C8.94,22,9.28,22,12,22s3.06,0,4.12-.06a7.3,7.3,0,0,0,2.43-.47,4.7,4.7,0,0,0,1.77-1.15,4.85,4.85,0,0,0,1.16-1.77,7.59,7.59,0,0,0,.46-2.43c0-1.06.06-1.4.06-4.12S22,8.94,21.94,7.88ZM20.14,16a5.61,5.61,0,0,1-.34,1.86,3.06,3.06,0,0,1-.75,1.15,3.19,3.19,0,0,1-1.15.75,5.61,5.61,0,0,1-1.86.34c-1,.05-1.37.06-4,.06s-3,0-4-.06A5.73,5.73,0,0,1,6.1,19.8,3.27,3.27,0,0,1,5,19.05a3,3,0,0,1-.74-1.15A5.54,5.54,0,0,1,3.86,16c0-1-.06-1.37-.06-4s0-3,.06-4A5.54,5.54,0,0,1,4.21,6.1,3,3,0,0,1,5,5,3.14,3.14,0,0,1,6.1,4.2,5.73,5.73,0,0,1,8,3.86c1,0,1.37-.06,4-.06s3,0,4,.06a5.61,5.61,0,0,1,1.86.34A3.06,3.06,0,0,1,19.05,5,3.06,3.06,0,0,1,19.8,6.1,5.61,5.61,0,0,1,20.14,8c.05,1,.06,1.37.06,4S20.19,15,20.14,16ZM12,6.87A5.13,5.13,0,1,0,17.14,12,5.12,5.12,0,0,0,12,6.87Zm0,8.46A3.33,3.33,0,1,1,15.33,12,3.33,3.33,0,0,1,12,15.33Z"></path></svg></a></li>
-              </ul>
-            </div>
-            <div className="col-12 order-md-3 col-md-8 col-lg-6 col-xl-4">
-              <div className="row">
-                <div className="col-12">
-                  <h6 className="footer__title">Browse</h6>
-                </div>
-                <div className="col-6">
-                  <ul className="footer__list">
-                    <li><a href="catalog1.html">Movies</a></li>
-                    <li><a href="catalog1.html">TV Shows</a></li>
-                    <li><a href="catalog1.html">Anime</a></li>
-                    <li><a href="catalog1.html">Cartoons</a></li>
-                  </ul>
-                </div>
-                <div className="col-6">
-                  <ul className="footer__list">
-                    <li><a href="catalog1.html">Netflix</a></li>
-                    <li><a href="catalog1.html">Marvel</a></li>
-                    <li><a href="catalog1.html">DC Comics</a></li>
-                    <li><a href="catalog1.html">Book adaptations</a></li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-            <div className="col-6 order-md-2 col-md-4 col-lg-3 col-xl-2">
-              <h6 className="footer__title">Resources</h6>
-              <ul className="footer__list">
-                <li><a href="about.html">About us</a></li>
-                <li><a href="pricing.html">Pricing plans</a></li>
-                <li><a href="faq.html">Help center</a></li>
-                <li><a href="contacts.html">Contacts</a></li>
-              </ul>
-            </div>
-            <div className="col-6 order-md-4 col-md-4 col-lg-3 col-xl-2">
-              <h6 className="footer__title">Help</h6>
-              <ul className="footer__list">
-                <li><a href="terms.html">Terms & Conditions</a></li>
-                <li><a href="privacy.html">Privacy Policy</a></li>
-                <li><a href="support.html">Support</a></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </footer>
-    </div>
-  );
+	return (
+		<footer className="footer">
+			<Container>
+				<Row>
+					<Col xs={12} md={8} lg={12} xl={4} className="order-md-1">
+						<div className="footer__logo">
+							<Image src="/images/logo/Flicko.png"alt="Logo" fluid />
+						</div>
+						<p className="footer__tagline">
+							Movies & TV Shows, Online cinema, <br /> Movie database HTML Template. <br /> The perfect choice for your project.
+						</p>
+					</Col>
+					<Col xs={12} md={8} lg={6} xl={4} className="order-md-3">
+						<Row>
+							<Col xs={12}>
+								<h6 className="footer__title">Browse</h6>
+							</Col>
+							<Col xs={6}>
+								<ul className="footer__list">
+									<li><a href="catalog1.html">Movies</a></li>
+									<li><a href="catalog1.html">TV Shows</a></li>
+									<li><a href="catalog1.html">Anime</a></li>
+									<li><a href="catalog1.html">Cartoons</a></li>
+								</ul>
+							</Col>
+							<Col xs={6}>
+								<ul className="footer__list">
+									<li><a href="catalog1.html">Netflix</a></li>
+									<li><a href="catalog1.html">Marvel</a></li>
+									<li><a href="catalog1.html">DC Comics</a></li>
+									<li><a href="catalog1.html">Book adaptations</a></li>
+								</ul>
+							</Col>
+						</Row>
+					</Col>
+					<Col xs={6} md={4} lg={3} xl={2} className="order-md-2">
+						<h6 className="footer__title">Resources</h6>
+						<ul className="footer__list">
+							<li><a href="about.html">About us</a></li>
+							<li><a href="pricing.html">Pricing plans</a></li>
+							<li><a href="faq.html">Help center</a></li>
+							<li><a href="contacts.html">Contacts</a></li>
+						</ul>
+					</Col>
+					<Col xs={6} md={4} lg={3} xl={2} className="order-md-4">
+						<h6 className="footer__title">Help</h6>
+						<ul className="footer__list">
+							<li><a href="terms.html">Terms & Conditions</a></li>
+							<li><a href="privacy.html">Privacy Policy</a></li>
+							<li><a href="support.html">Support</a></li>
+						</ul>
+					</Col>
+				</Row>
+			</Container>
+		</footer>
+	);
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,159 +1,87 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button, Container } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+// Uncomment the import statement below to use the CDN version of Bootstrap if needed
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default function Navbar() {
+export default function NavbarComponent() {
     return (
-        <div>
-            <header className="header">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-12">
-                            <div className="header__content">
-                                <a href="index.html" className="header__logo">
-                                    <img src="/images/logo/Flicko.png" alt="Logo" className="logo-img" />
-                                </a>
-
-                                <div className="header__categories">
-                                    <button className="header__categories-btn" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                        <span></span>
-                                        <span></span>
-                                    </button>
-                                    <div className="dropdown-menu header__dropdown-menu header__dropdown-menu--categories">
-                                        <ul className="header__categories-list">
-                                            <li><a href="catalog1.html">Films</a></li>
-                                            <li><a href="catalog2.html">TV Series</a></li>
-                                            <li><a href="catalog1.html">Anime</a></li>
-                                            <li><a href="catalog2.html">Cartoons</a></li>
-                                        </ul>
-                                        <ul className="header__categories-list">
-                                            <li><a href="catalog1.html">Catalog Grid</a></li>
-                                            <li><a href="catalog2.html">Catalog List</a></li>
-                                            <li><a href="details1.html">Details Film</a></li>
-                                            <li><a href="details2.html">Details TV Series</a></li>
-                                        </ul>
-                                    </div>
-                                </div>
-                                <ul className="header__nav">
-                                    <li className="header__nav-item">
-                                        <a className="header__nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Home <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M17,9.17a1,1,0,0,0-1.41,0L12,12.71,8.46,9.17a1,1,0,0,0-1.41,0,1,1,0,0,0,0,1.42l4.24,4.24a1,1,0,0,0,1.42,0L17,10.59A1,1,0,0,0,17,9.17Z" /></svg></a>
-                                        <ul className="dropdown-menu header__dropdown-menu">
-                                            <li><a href="index.html">Home style 1</a></li>
-                                            <li><a href="index2.html">Home style 2</a></li>
-                                        </ul>
-                                    </li>
-                                    <li className="header__nav-item">
-                                        <a className="header__nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Catalog <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M17,9.17a1,1,0,0,0-1.41,0L12,12.71,8.46,9.17a1,1,0,0,0-1.41,0,1,1,0,0,0,0,1.42l4.24,4.24a1,1,0,0,0,1.42,0L17,10.59A1,1,0,0,0,17,9.17Z" /></svg></a>
-                                        <ul className="dropdown-menu header__dropdown-menu">
-                                            <li><a href="catalog1.html">Catalog Grid</a></li>
-                                            <li><a href="catalog2.html">Catalog List</a></li>
-                                            <li><a href="details1.html">Details Movie</a></li>
-                                            <li><a href="details2.html">Details TV Series</a></li>
-                                        </ul>
-                                    </li>
-                                    <li className="header__nav-item">
-                                        <a href="pricing.html" className="header__nav-link">Pricing plans</a>
-                                    </li>
-                                    <li className="header__nav-item">
-                                        <a className="header__nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Pages <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M17,9.17a1,1,0,0,0-1.41,0L12,12.71,8.46,9.17a1,1,0,0,0-1.41,0,1,1,0,0,0,0,1.42l4.24,4.24a1,1,0,0,0,1.42,0L17,10.59A1,1,0,0,0,17,9.17Z" /></svg></a>
-                                        <ul className="dropdown-menu header__dropdown-menu">
-                                            <li><a href="about.html">About us</a></li>
-                                            <li><a href="faq.html">Help center</a></li>
-                                            <li><a href="profile.html">Profile</a></li>
-                                            <li><a href="actor.html">Actor</a></li>
-                                            <li><a href="contacts.html">Contacts</a></li>
-                                            <li><a href="privacy.html">Privacy policy</a></li>
-                                        </ul>
-                                    </li>
-                                    <li className="header__nav-item">
-                                        <a className="header__nav-link header__nav-link--more" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12,10a2,2,0,1,0,2,2A2,2,0,0,0,12,10ZM5,10a2,2,0,1,0,2,2A2,2,0,0,0,5,10Zm14,0a2,2,0,1,0,2,2A2,2,0,0,0,19,10Z" /></svg>
-                                        </a>
-                                        <ul className="dropdown-menu header__dropdown-menu">
-                                            <li><a href="signin.html">Sign In</a></li>
-                                            <li><a href="signup.html">Sign Up</a></li>
-                                            <li><a href="forgot.html">Forgot password</a></li>
-                                            <li><a href="404.html">404 Page</a></li>
-                                        </ul>
-                                    </li>
-                                </ul>
-                                <div className="header__actions">
-                                    <form action="#" className="header__search">
-                                        <input type="text" placeholder="Search" />
-                                        <button type="button"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21.71,20.29,18,16.61A9,9,0,1,0,16.61,18l3.68,3.68a1,1,0,0,0,1.42,0A1,1,0,0,0,21.71,20.29ZM11,18a7,7,0,1,1,7-7A7,7,0,0,1,11,18Z" /></svg></button>
-                                    </form>
-                                    <div className="header__language">
-                                        <a className="header__nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">EN <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M17,9.17a1,1,0,0,0-1.41,0L12,12.71,8.46,9.17a1,1,0,0,0-1.41,0,1,1,0,0,0,0,1.42l4.24,4.24a1,1,0,0,0,1.42,0L17,10.59A1,1,0,0,0,17,9.17Z" /></svg></a>
-                                        <ul className="dropdown-menu header__dropdown-menu header__dropdown-menu--lang">
-                                            <li><a href="#">English</a></li>
-                                            <li><a href="#">Spanish</a></li>
-                                            <li><a href="#">French</a></li>
-                                        </ul>
-                                    </div>
-                                    <a href="signin.html" className="header__sign-in">
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M20,12a1,1,0,0,0-1-1H11.41l2.3-2.29a1,1,0,1,0-1.42-1.42l-4,4a1,1,0,0,0-.21.33,1,1,0,0,0,0,.76,1,1,0,0,0,.21.33l4,4a1,1,0,0,0,1.42,0,1,1,0,0,0,0-1.42L11.41,13H19A1,1,0,0,0,20,12ZM17,2H7A3,3,0,0,0,4,5V19a3,3,0,0,0,3,3H17a3,3,0,0,0,3-3V16a1,1,0,0,0-2,0v3a1,1,0,0,1-1,1H7a1,1,0,0,1-1-1V5A1,1,0,0,1,7,4H17a1,1,0,0,1,1,1V8a1,1,0,0,0,2,0V5A3,3,0,0,0,17,2Z" /></svg>
-                                        <span>sign in</span>
-                                    </a>
-                                </div>
-                                <button className="header__btn" type="button">
-                                    <span></span>
-                                    <span></span>
-                                    <span></span>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </header>
-            <div class="menu">
-                <form action="#" class="menu__search">
-                    <input type="text" placeholder="Search"></input>
-                    <button type="button"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21.71,20.29,18,16.61A9,9,0,1,0,16.61,18l3.68,3.68a1,1,0,0,0,1.42,0A1,1,0,0,0,21.71,20.29ZM11,18a7,7,0,1,1,7-7A7,7,0,0,1,11,18Z"></path></svg></button>
-                </form>
-                <ul class="menu__nav">
-                    <li class="menu__nav-item">
-                        <a class="menu__nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Home <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M17,9.17a1,1,0,0,0-1.41,0L12,12.71,8.46,9.17a1,1,0,0,0-1.41,0,1,1,0,0,0,0,1.42l4.24,4.24a1,1,0,0,0,1.42,0L17,10.59A1,1,0,0,0,17,9.17Z" /></svg></a>
-                        <ul class="dropdown-menu menu__dropdown-menu">
-                            <li><a href="index.html">Home style 1</a></li>
-                            <li><a href="index2.html">Home style 2</a></li>
-                        </ul>
-                    </li>
-                    <li class="menu__nav-item">
-                        <a class="menu__nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Catalog <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M17,9.17a1,1,0,0,0-1.41,0L12,12.71,8.46,9.17a1,1,0,0,0-1.41,0,1,1,0,0,0,0,1.42l4.24,4.24a1,1,0,0,0,1.42,0L17,10.59A1,1,0,0,0,17,9.17Z" /></svg></a>
-                        <ul class="dropdown-menu menu__dropdown-menu">
-                            <li><a href="catalog1.html">Catalog Grid</a></li>
-                            <li><a href="catalog2.html">Catalog List</a></li>
-                            <li><a href="details1.html">Details Movie</a></li>
-                            <li><a href="details2.html">Details TV Series</a></li>
-                        </ul>
-                    </li>
-                    <li class="menu__nav-item">
-                        <a href="pricing.html" class="menu__nav-link">Pricing plans</a>
-                    </li>
-                    <li class="menu__nav-item">
-                        <a class="menu__nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Pages <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M17,9.17a1,1,0,0,0-1.41,0L12,12.71,8.46,9.17a1,1,0,0,0-1.41,0,1,1,0,0,0,0,1.42l4.24,4.24a1,1,0,0,0,1.42,0L17,10.59A1,1,0,0,0,17,9.17Z" /></svg></a>
-                        <ul class="dropdown-menu menu__dropdown-menu">
-                            <li><a href="about.html">About us</a></li>
-                            <li><a href="faq.html">Help center</a></li>
-                            <li><a href="profile.html">Profile</a></li>
-                            <li><a href="actor.html">Actor</a></li>
-                            <li><a href="contacts.html">Contacts</a></li>
-                            <li><a href="privacy.html">Privacy policy</a></li>
-                        </ul>
-                    </li>
-                    <li class="menu__nav-item">
-                        <a class="menu__nav-link menu__nav-link--more" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12,10a2,2,0,1,0,2,2A2,2,0,0,0,12,10ZM5,10a2,2,0,1,0,2,2A2,2,0,0,0,5,10Zm14,0a2,2,0,1,0,2,2A2,2,0,0,0,19,10Z" /></svg>
-                        </a>
-                        <ul class="dropdown-menu menu__dropdown-menu">
-                            <li><a href="signin.html">Sign In</a></li>
-                            <li><a href="signup.html">Sign Up</a></li>
-                            <li><a href="forgot.html">Forgot password</a></li>
-                            <li><a href="404.html">404 Page</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-        </div>
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <LinkContainer to="/">
+                    <Navbar.Brand>
+                        <img src="/images/logo/Flicko.png" alt="Logo" className="logo-img" />
+                    </Navbar.Brand>
+                </LinkContainer>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <NavDropdown title="Home" id="home-dropdown">
+                            <LinkContainer to="/index.html">
+                                <NavDropdown.Item>Home style 1</NavDropdown.Item>
+                            </LinkContainer>
+                            <LinkContainer to="/index2.html">
+                                <NavDropdown.Item>Home style 2</NavDropdown.Item>
+                            </LinkContainer>
+                        </NavDropdown>
+                        <NavDropdown title="Catalog" id="catalog-dropdown">
+                            <LinkContainer to="/catalog1.html">
+                                <NavDropdown.Item>Catalog Grid</NavDropdown.Item>
+                            </LinkContainer>
+                            <LinkContainer to="/catalog2.html">
+                                <NavDropdown.Item>Catalog List</NavDropdown.Item>
+                            </LinkContainer>
+                            <LinkContainer to="/details1.html">
+                                <NavDropdown.Item>Details Movie</NavDropdown.Item>
+                            </LinkContainer>
+                            <LinkContainer to="/details2.html">
+                                <NavDropdown.Item>Details TV Series</NavDropdown.Item>
+                            </LinkContainer>
+                        </NavDropdown>
+                        <LinkContainer to="/pricing.html">
+                            <Nav.Link>Pricing plans</Nav.Link>
+                        </LinkContainer>
+                        <NavDropdown title="Pages" id="pages-dropdown">
+                            <LinkContainer to="/about.html">
+                                <NavDropdown.Item>About us</NavDropdown.Item>
+                            </LinkContainer>
+                            <LinkContainer to="/faq.html">
+                                <NavDropdown.Item>Help center</NavDropdown.Item>
+                            </LinkContainer>
+                            <LinkContainer to="/profile.html">
+                                <NavDropdown.Item>Profile</NavDropdown.Item>
+                            </LinkContainer>
+                            <LinkContainer to="/actor.html">
+                                <NavDropdown.Item>Actor</NavDropdown.Item>
+                            </LinkContainer>
+                            <LinkContainer to="/contacts.html">
+                                <NavDropdown.Item>Contacts</NavDropdown.Item>
+                            </LinkContainer>
+                            <LinkContainer to="/privacy.html">
+                                <NavDropdown.Item>Privacy policy</NavDropdown.Item>
+                            </LinkContainer>
+                        </NavDropdown>
+                        <NavDropdown title="More" id="more-dropdown">
+                            <LinkContainer to="/signin.html">
+                                <NavDropdown.Item>Sign In</NavDropdown.Item>
+                            </LinkContainer>
+                            <LinkContainer to="/signup.html">
+                                <NavDropdown.Item>Sign Up</NavDropdown.Item>
+                            </LinkContainer>
+                            <LinkContainer to="/forgot.html">
+                                <NavDropdown.Item>Forgot password</NavDropdown.Item>
+                            </LinkContainer>
+                            <LinkContainer to="/404.html">
+                                <NavDropdown.Item>404 Page</NavDropdown.Item>
+                            </LinkContainer>
+                        </NavDropdown>
+                    </Nav>
+                    <Form className="d-flex">
+                        <FormControl type="search" placeholder="Search" className="me-2" aria-label="Search" />
+                        <Button variant="outline-success">Search</Button>
+                    </Form>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 }
